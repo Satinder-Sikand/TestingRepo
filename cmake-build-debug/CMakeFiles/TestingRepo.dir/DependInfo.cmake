142f33ea7b64e99feb144819c6ad0de71ed57988
satinder-sikand <satinder.sikand@caseware.com>
2020-07-28 16:10:27 -0400

    added cmake

---
+++ cmake-build-debug/CMakeFiles/TestingRepo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/satinder.sikand/source/repos/TestingRepo/Source.cpp" "C:/Users/satinder.sikand/source/repos/TestingRepo/cmake-build-debug/CMakeFiles/TestingRepo.dir/Source.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TestingRepo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestingRepo.dir/Source.cpp.obj"
+  "TestingRepo.exe"
+  "TestingRepo.exe.manifest"
+  "TestingRepo.lib"
+  "TestingRepo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestingRepo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
